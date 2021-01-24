f6c48267f2f80db0b794459ffc9a9163f6143c98
vspaz <vladimir.edelman@gmail.com>
2021-01-24 15:32:30 +0100

    extend the test

--- designpatterns/abastractfactory/carfactory_test.go
+++ designpatterns/abastractfactory/carfactory_test.go
@@ -9,4 +9,5 @@ func TestAbstractFactory(t *testing.T) {
 	audiFactory, _ := getCarAbstractFactory("audi")
 	audiSedan := audiFactory.MakeSedan()
 	assert.Equal(t, "A8", audiSedan.GetMake())
+	assert.Equal(t, 70000, audiSedan.GetPriceTag())
 }
