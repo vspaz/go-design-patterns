a11e0f341ed5fdacf9eac2e301589c681297d1b1
vspaz <vladimir.edelman@gmail.com>
2021-01-24 15:35:49 +0100

    add another test

--- designpatterns/abastractfactory/carfactory_test.go
+++ designpatterns/abastractfactory/carfactory_test.go
@@ -5,9 +5,15 @@ import (
 	"testing"
 )
 
-func TestAbstractFactory(t *testing.T) {
+func TestGetCarAbstractFactoryOk(t *testing.T) {
 	audiFactory, _ := getCarAbstractFactory("audi")
 	audiSedan := audiFactory.MakeSedan()
 	assert.Equal(t, "A8", audiSedan.GetMake())
 	assert.Equal(t, 70000, audiSedan.GetPriceTag())
+}
+
+func TestGetCarAbstractFactoryNotImplementedType(t *testing.T) {
+audiFactory, err := getCarAbstractFactory("tesla")
+    assert.Equal(t, nil, audiFactory)
+    assert.NotEqual(t, nil, err)
 }
