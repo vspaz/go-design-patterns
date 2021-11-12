25d99dc4e73c5b23166bde3db6d7b02170839a7b
vspaz <vladimir.edelman@gmail.com>
2021-11-12 18:22:05 +0100

    add 'Build' method

--- designpatterns/creational/builder/builder.go
+++ designpatterns/creational/builder/builder.go
@@ -1,16 +1,12 @@
 package builder
 
 type IBuilder interface {
-	WithWheels() IBuilder
-	WithSeats() IBuilder
-	WithChasis() IBuilder
-	WithGasEngine() IBuilder
-	WithElectricalMotor() IBuilder
+	WithChasis(chasis string) IBuilder
+	WithGasEngine(isGas bool) IBuilder
+	WithElectricalMotor(isElectrical bool) IBuilder
 }
 
 type CarBuilder struct {
-	wheels          int
-	seats           int
 	chasis          string
 	gasEngine       bool
 	electricalMotor bool
@@ -34,28 +30,18 @@ type Car struct {
 	price     float64
 }
 
-func (c *CarBuilder) WithSeats() IBuilder {
-	c.seats = 4
+func (c *CarBuilder) WithChasis(chasis string) IBuilder {
+	c.chasis = chasis
 	return c
 }
 
-func (c *CarBuilder) WithChasis() IBuilder {
-	c.chasis = "aluminium"
+func (c *CarBuilder) WithGasEngine(isGas bool) IBuilder {
+	c.gasEngine = isGas
 	return c
 }
 
-func (c *CarBuilder) WithGasEngine() IBuilder {
-	c.gasEngine = false
-	return c
-}
-
-func (c *CarBuilder) WithElectricalMotor() IBuilder {
-	c.electricalMotor = true
-	return c
-}
-
-func (c *CarBuilder) WithWheels() IBuilder {
-	c.wheels = 4
+func (c *CarBuilder) WithElectricalMotor(isElectrical bool) IBuilder {
+	c.electricalMotor = isElectrical
 	return c
 }
 
@@ -65,8 +51,6 @@ func (c *CarBuilder) Build() Car {
 		Make:      "Portofino",
 		Brand:     "Ferrari",
 		Specs: Specs{
-			Wheels:          c.wheels,
-			Seats:           c.seats,
 			Chasis:          c.chasis,
 			ElectricalMotor: c.electricalMotor,
 		},
