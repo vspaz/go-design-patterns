bd8995bd180ae3aeac80b977bb3ffd9c88132b27
vspaz <vladimir.edelman@gmail.com>
2021-11-12 18:02:51 +0100

    add method stubs

--- designpatterns/creational/builder/builder.go
+++ designpatterns/creational/builder/builder.go
@@ -5,5 +5,34 @@ type IBuilder interface {
 	WithSeats() IBuilder
 	WithChasis() IBuilder
 	WithGasEngine() IBuilder
-	WithElectricMotor() IBuilder
+	WithElectricalMotor() IBuilder
+}
+
+type CarBuilder struct {
+	wheels          int
+	seats           int
+	chasis          string
+	gasEngine       bool
+	electricalMotor bool
+}
+
+func (c *CarBuilder) WithSeats() IBuilder {
+	panic("implement me")
+}
+
+func (c *CarBuilder) WithChasis() IBuilder {
+	panic("implement me")
+}
+
+func (c *CarBuilder) WithGasEngine() IBuilder {
+	panic("implement me")
+}
+
+func (c *CarBuilder) WithElectricalMotor() IBuilder {
+	panic("implement me")
+}
+
+func (c *CarBuilder) WithWheels() IBuilder {
+	c.wheels = 4
+	return c
 }
