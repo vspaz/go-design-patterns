5ce03277c75b444d3f80952c0292ffee38a924a7
vspaz <vladimir.edelman@gmail.com>
2021-11-14 13:12:52 +0100

    add 'TestGetSingletonOk'

--- designpatterns/creational/singleton/singleton_test.go
+++ designpatterns/creational/singleton/singleton_test.go
@@ -1,7 +1,16 @@
 package singleton
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestGetSingletonOk(t *testing.T) {
+	singleton_1 := getSingleton()
+	assert.Equal(t, singleton_1.CreateCount, 1)
+	assert.Equal(t, singleton_1.MethodInvocationCount, 1)
 
+	singleton_2 := getSingleton()
+	assert.Equal(t, singleton_2.CreateCount, 1)
+	assert.Equal(t, singleton_2.MethodInvocationCount, 2)
 }
