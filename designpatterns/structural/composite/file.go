f9554ae3126053593384cc9ce6ab7d2ec71ae763
vspaz <vladimir.edelman@gmail.com>
2021-11-15 19:48:11 +0100

    add file.File.search

--- designpatterns/structural/composite/file.go
+++ designpatterns/structural/composite/file.go
@@ -1,5 +1,12 @@
 package composite
 
+import "fmt"
+
 type File struct {
 	name string
 }
+
+func (f *File) search(word string) string {
+	return fmt.Sprintf("searching for word '%s' in file '%s'", word, f.name)
+}
+
