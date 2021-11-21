03ca5f8e0ca7e101d2216454936c232072dec3b2
vspaz <vladimir.edelman@gmail.com>
2021-11-21 11:01:08 +0100

    add 'dummy request handler'

--- designpatterns/structural/proxy/webapp.go
+++ designpatterns/structural/proxy/webapp.go
@@ -1,3 +1,14 @@
 package proxy
 
+import "net/http"
+
 type WebApp struct{}
+
+func (w *WebApp) handleRequest(endpoint string, method string) (int, string) {
+	if endpoint == "/api/v1/status" && method == http.MethodHead {
+		return http.StatusOK, "Ok"
+	} else if endpoint == "/api/v1/user" && method == http.MethodPost {
+		return http.StatusAccepted, "Accepted"
+	}
+	return http.StatusBadRequest, "Bad request"
+}
