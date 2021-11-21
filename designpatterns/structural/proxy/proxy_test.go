b4fca8b060264ab1d0d7506c4c8a6a6b997fb314
vspaz <vladimir.edelman@gmail.com>
2021-11-21 11:29:31 +0100

    add test case

--- designpatterns/structural/proxy/proxy_test.go
+++ designpatterns/structural/proxy/proxy_test.go
@@ -1,7 +1,19 @@
 package proxy
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+const (
+	statusEndpoint = "/api/v1/status"
+	userEndpoint   = "/api/v1/user"
+)
 
 func TestProxyServer(t *testing.T) {
-
+	proxyServer := NewProxyServer(2)
+	statusCode, response := proxyServer.handleRequest(statusEndpoint, "GET")
+	assert.Equal(t, http.StatusOK, statusCode)
+	assert.Equal(t, "Ok", response)
 }
--- designpatterns/structural/proxy/webapp.go
+++ designpatterns/structural/proxy/webapp.go
@@ -5,7 +5,7 @@ import "net/http"
 type WebApp struct{}
 
 func (w *WebApp) handleRequest(endpoint string, method string) (int, string) {
-	if endpoint == "/api/v1/status" && method == http.MethodHead {
+	if endpoint == "/api/v1/status" && method == http.MethodGet {
 		return http.StatusOK, "Ok"
 	} else if endpoint == "/api/v1/user" && method == http.MethodPost {
 		return http.StatusAccepted, "Accepted"
