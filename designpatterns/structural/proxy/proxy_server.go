e50eac3b7635b9ab1602e0865847cc98316bd9c2
vspaz <vladimir.edelman@gmail.com>
2021-11-21 11:08:13 +0100

    add constructor 'NewProxyServer'

--- designpatterns/structural/proxy/proxy_server.go
+++ designpatterns/structural/proxy/proxy_server.go
@@ -5,3 +5,11 @@ type ProxyServer struct {
 	RequestCount int
 	RateLimiter  map[string]int
 }
+
+func NewProxyServer() *ProxyServer {
+	return &ProxyServer{
+		AppServer:    &WebApp{},
+		RequestCount: 5,
+		RateLimiter:  make(map[string]int),
+	}
+}
