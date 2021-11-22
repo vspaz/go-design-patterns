41cc55d7795dc8d410886933b6d50c3ed44e7b33
vspaz <vladimir.edelman@gmail.com>
2021-11-22 18:08:18 +0100

    add 'profiling.go'

--- designpatterns/structural/decorator/profiling.go
+++ designpatterns/structural/decorator/profiling.go
@@ -1,1 +1,9 @@
 package decorator
+
+type ProfileRequest struct {
+	Request Request
+}
+
+func (p *ProfileRequest) DoRequest() string {
+	return "request took: '0.02ms to process'; " + p.DoRequest()
+}
