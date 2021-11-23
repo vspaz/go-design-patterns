93ee9f00cf13b2029e1afac03b154bba049ea48b
vspaz <vladimir.edelman@gmail.com>
2021-11-23 17:57:53 +0100

    add 'AddNewEntry'

--- designpatterns/structural/facade/ledger.go
+++ designpatterns/structural/facade/ledger.go
@@ -1,4 +1,12 @@
 package facade
 
+import "fmt"
+
 type Ledger struct {
+}
+
+func (l *Ledger) AddNewEntry(accountId string, transactionType string, amount int) string {
+	return fmt.Sprintf("new entry for account id '%s'; transaction type '%s'; amount %d",
+		accountId, transactionType, amount,
+	)
 }
