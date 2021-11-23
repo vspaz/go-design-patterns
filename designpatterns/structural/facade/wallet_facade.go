f9dd637ee2d136a716682e4c7d8b9854eccca29b
vspaz <vladimir.edelman@gmail.com>
2021-11-23 18:18:00 +0100

    add 'w.Withraw'

--- designpatterns/structural/facade/wallet_facade.go
+++ designpatterns/structural/facade/wallet_facade.go
@@ -35,3 +35,16 @@ func (w *WalletFacade) Deposit(accountId string, securityCode int, amount int) s
 	operation_3 := w.Ledger.AddNewEntry(accountId, "deposit", amount)
 	return strings.Join([]string{operation_1, operation_2, operation_3}, " ")
 }
+
+func (w *WalletFacade) Withdraw(accountId string, securityCode int, amount int) string {
+	if !w.Account.IsValid(accountId) {
+		return fmt.Sprintf("account '%s' does not exist")
+	}
+	if !w.SecurityCode.IsAuthorizationCodeCorrect(securityCode) {
+		return fmt.Sprintf("security code invalid")
+	}
+	operation_1 := w.Wallet.Withdraw(amount)
+	operation_2 := w.Notification.NotifyWithdrawalTransaction()
+	operation_3 := w.Ledger.AddNewEntry(accountId, "withdrawal", amount)
+	return strings.Join([]string{operation_1.Error(), operation_2, operation_3}, " ")
+}
