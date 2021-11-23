4cb827e4686f627b2a0eb87495172ec28de9f9bb
vspaz <vladimir.edelman@gmail.com>
2021-11-23 18:12:10 +0100

    add w.Deposit

--- designpatterns/structural/facade/wallet_facade.go
+++ designpatterns/structural/facade/wallet_facade.go
@@ -1,5 +1,10 @@
 package facade
 
+import (
+	"fmt"
+	"strings"
+)
+
 type WalletFacade struct {
 	Account      *Account
 	Wallet       *Wallet
@@ -16,4 +21,17 @@ func NewWalletFacade(accountId string, code int) *WalletFacade {
 		Notification: NewNotification(),
 		Ledger:       NewLedger(),
 	}
+}
+
+func (w *WalletFacade) Deposit(accountId string, securityCode int, amount int) string {
+	if !w.Account.IsValid(accountId) {
+		return fmt.Sprintf("account '%s' does not exist")
+	}
+	if !w.SecurityCode.IsAuthorizationCodeCorrect(securityCode) {
+		return fmt.Sprintf("security code invalid")
+	}
+	operation_1 := w.Wallet.Add(amount)
+	operation_2 := w.Notification.NotifyDepositTransaction()
+	operation_3 := w.Ledger.AddNewEntry(accountId, "deposit", amount)
+	return strings.Join([]string{operation_1, operation_2, operation_3}, " ")
 }
