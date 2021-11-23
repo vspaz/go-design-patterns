94ec88698a92fc09f5e1425a1993ab987152202a
vspaz <vladimir.edelman@gmail.com>
2021-11-23 17:40:17 +0100

    add wallet

--- designpatterns/structural/facade/wallet.go
+++ designpatterns/structural/facade/wallet.go
@@ -1,1 +1,15 @@
 package facade
+
+type Wallet struct {
+	Balance int
+}
+
+func NewWallet() *Wallet {
+	return &Wallet{
+		Balance: 0,
+	}
+}
+
+func (w *Wallet) Add(amount int) {
+	w.Balance += amount
+}
