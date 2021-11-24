1bebc89827b223dfeaead7f8d2734e51595bdc04
vspaz <vladimir.edelman@gmail.com>
2021-11-24 16:45:15 +0100

    add 'HasNext' and 'Next'

--- designpatterns/behavioral/iterator/iterator.go
+++ designpatterns/behavioral/iterator/iterator.go
@@ -2,5 +2,5 @@ package iterator
 
 type Iterator interface {
 	HasNext() bool
-	GetNext() bool
+	Next() bool
 }
--- designpatterns/behavioral/iterator/users.go
+++ designpatterns/behavioral/iterator/users.go
@@ -4,3 +4,16 @@ type UserIterator struct {
 	index int
 	users []*User
 }
+
+func (u *UserIterator) HasNext() bool {
+	return u.index < len(u.users)
+}
+
+func (u *UserIterator) Next() *User {
+	if u.HasNext() {
+		user := u.users[u.index]
+		u.index++
+		return user
+	}
+	return nil
+}
