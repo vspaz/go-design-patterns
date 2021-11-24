8a600d6cbb7e01c06258875342a7070107eb7b59
vspaz <vladimir.edelman@gmail.com>
2021-11-24 17:07:21 +0100

    add basic test

--- designpatterns/behavioral/iterator/iterator_test.go
+++ designpatterns/behavioral/iterator/iterator_test.go
@@ -1,13 +1,31 @@
 package iterator
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestIteratorOk(t *testing.T) {
-	users := &UserCollection{[]*User{
-		{
-			Name: "noname",
-			Age:  30,
+	users := &UserCollection{
+		[]*User{
+			{
+				Name: "noname",
+				Age:  30,
+			},
+			{
+				Name: "anynymous",
+				Age:  10,
+			},
 		},
-	},
 	}
+
+	iterator := users.CreateIterator()
+	count := 0
+	for iterator.HasNext() {
+		user := iterator.Next()
+		fmt.Println(user.Name)
+		count++
+	}
+	assert.Equal(t, 2, count)
 }
