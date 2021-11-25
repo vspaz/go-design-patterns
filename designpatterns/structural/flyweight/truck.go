8d6b18c6fed81406cd42c97ca844bb05a8cf768d
vspaz <vladimir.edelman@gmail.com>
2021-11-25 19:21:05 +0100

    add truck.go

--- designpatterns/structural/flyweight/truck.go
+++ designpatterns/structural/flyweight/truck.go
@@ -1,1 +1,17 @@
 package flyweight
+
+import "fmt"
+
+type Truck struct {
+	carType string
+}
+
+func NewTruck(carType string) *Truck {
+	return &Truck{
+		carType: carType,
+	}
+}
+
+func (t *Truck) GetInfo(color string) string {
+	return fmt.Sprintf("'%s' of color '%s' is created", t.carType, color)
+}
