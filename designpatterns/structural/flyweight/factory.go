d63b10864d87efc4e717579271b2f8048409b2a9
vspaz <vladimir.edelman@gmail.com>
2021-11-25 18:58:44 +0100

    add GetTeam method

--- designpatterns/structural/flyweight/factory.go
+++ designpatterns/structural/flyweight/factory.go
@@ -1,5 +1,31 @@
 package flyweight
 
-type teamFactory struct {
-	team map[string]*Team
+func getTeam(team int) *Team {
+	switch team {
+	case 1:
+		return &Team{
+			Id:   1,
+			Name: "idToTeams 1",
+		}
+	case 2:
+		return &Team{
+			Id:   2,
+			Name: "idToTeams 2",
+		}
+	default:
+		return nil
+	}
+}
+
+type Teams struct {
+	idToTeams map[int]*Team
+}
+
+func (t *Teams) GetTeam(teamId int) *Team {
+	if t.idToTeams[teamId] != nil {
+		return t.idToTeams[teamId]
+	}
+	team := getTeam(teamId)
+	t.idToTeams[teamId] = getTeam(teamId)
+	return team
 }
