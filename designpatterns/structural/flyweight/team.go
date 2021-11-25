2e955cffe57a20c83508ccbad160c629caef3f60
vspaz <vladimir.edelman@gmail.com>
2021-11-25 18:44:25 +0100

    add 'Game'

--- designpatterns/structural/flyweight/team.go
+++ designpatterns/structural/flyweight/team.go
@@ -1,9 +1,20 @@
 package flyweight
 
+import "time"
+
 type Player struct {
-	Name string
-	LastName string
+	Name         string
+	LastName     string
 	PreviousTeam uint64
-	Pic []byte
+	Pic          []byte
 }
 
+type Game struct {
+	Date          time.Time
+	VisitorId     uint64
+	LocalId       uint64
+	LocalScore    byte
+	VisitorScore  byte
+	LocalShoots   uint64
+	VisitorShoots uint64
+}
