2bf8c33e9857749b7d6e3ed52e9f901853d42389
vspaz <vladimir.edelman@gmail.com>
2021-11-25 19:23:48 +0100

    add 'struct' Suv

--- designpatterns/structural/flyweight/suv.go
+++ designpatterns/structural/flyweight/suv.go
@@ -1,1 +1,17 @@
 package flyweight
+
+import "fmt"
+
+type Suv struct {
+	carType string
+}
+
+func NewSuv(carType string) *Suv {
+	return &Suv{
+		carType: carType,
+	}
+}
+
+func (t *Suv) GetInfo(color string) string {
+	return fmt.Sprintf("'%s' of color '%s' is created", t.carType, color)
+}
