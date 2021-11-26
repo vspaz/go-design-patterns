5141b59b77bf79028ce18e2fcc5e615834c79255
vspaz <vladimir.edelman@gmail.com>
2021-11-26 20:41:46 +0100

    client code

--- designpatterns/behavioral/strategy/cache.go
+++ designpatterns/behavioral/strategy/cache.go
@@ -6,3 +6,32 @@ type Cache struct {
 	Capacity       int
 	MaxCapacity    int
 }
+
+func initCache(policy evictionPolicy) *Cache {
+	storage := make(map[string]string)
+	return &Cache{
+		Storage:        storage,
+		EvictionPolicy: policy,
+		Capacity:       0,
+		MaxCapacity:    5,
+	}
+}
+
+func (c *Cache) setEvictionPolicy(e evictionPolicy) {
+	c.EvictionPolicy = e
+}
+
+func (c *Cache) evict() {
+	c.EvictionPolicy.evict(c)
+	c.Capacity--
+}
+
+func (c *Cache) Add(key string, value string) {
+	if c.Capacity == c.MaxCapacity {
+		c.evict()
+	}
+}
+
+func (c *Cache) Get(key string) {
+	delete(c.Storage, key)
+}
