5432dc6b83f4f52dc632556ce385e851b680d9a0
vspaz <vladimir.edelman@gmail.com>
2021-11-30 22:15:14 +0100

    add Logging methods

--- designpatterns/behavioral/chor/middleware/logging.go
+++ designpatterns/behavioral/chor/middleware/logging.go
@@ -1,5 +1,19 @@
 package middleware
 
+import (
+	"log"
+	"time"
+)
+
 type Logging struct {
 	next Middleware
+}
+
+func (l *Logging) Process(r *Request) {
+	log.Printf(" %s: request received", time.Now().Format("2006-01-02 15:04:05.000"))
+	l.next.Process(r)
+}
+
+func (l *Logging) setNext(next Middleware) {
+	l.next = next
 }
