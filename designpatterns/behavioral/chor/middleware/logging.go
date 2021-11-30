af0065ae7e1f5844d7572130ab78380004015c0d
vspaz <vladimir.edelman@gmail.com>
2021-11-30 22:00:28 +0100

    add logging.go

---
+++ designpatterns/behavioral/chor/middleware/logging.go
@@ -0,0 +1,5 @@
+package middleware
+
+type Logging struct {
+	next Middleware
+}
--- designpatterns/behavioral/chor/middleware/throttling.go
+++ designpatterns/behavioral/chor/middleware/throttling.go
@@ -2,14 +2,14 @@ package middleware
 
 import "time"
 
-type ThrottlingMwe struct {
+type Throttling struct {
 	requestCount int
 	rpsLimit     int
 	currentTime  time.Time
 	next         Middleware
 }
 
-func (t *ThrottlingMwe) Process(r *Request) string {
+func (t *Throttling) Process(r *Request) string {
 	if time.Now().Second() > t.currentTime.Second()+60 {
 		t.requestCount = 0
 		t.currentTime = time.Now()
@@ -22,6 +22,6 @@ func (t *ThrottlingMwe) Process(r *Request) string {
 	return t.next.Process(r)
 }
 
-func (t *ThrottlingMwe) SetNext(next Middleware) {
+func (t *Throttling) SetNext(next Middleware) {
 	t.next = next
 }
