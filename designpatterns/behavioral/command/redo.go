15ed09e4f33ce2cd68738478f9d9b6593e6af8a4
vspaz <vladimir.edelman@gmail.com>
2021-12-03 20:58:16 +0100

    add missing methods to Redo struct

--- designpatterns/behavioral/command/redo.go
+++ designpatterns/behavioral/command/redo.go
@@ -3,3 +3,11 @@ package command
 type Redo struct {
 	Receiver Receiver
 }
+
+func (r *Redo) Redo(receiver Receiver) {
+	r.Receiver = receiver
+}
+
+func (r *Redo) RunCommand() string {
+	return r.Receiver.RunRedo()
+}
