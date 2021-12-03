1a416a48a912c1c9ba3bf7e911c6083b44066a80
vspaz <vladimir.edelman@gmail.com>
2021-12-03 21:08:00 +0100

    extend the test

--- designpatterns/behavioral/command/command_test.go
+++ designpatterns/behavioral/command/command_test.go
@@ -12,4 +12,8 @@ func TestInvoker_RunCommand(t *testing.T) {
 		command: undoCmd,
 	}
 	assert.Equal(t, "'undo' run by receiver", caller.RunCommand())
+
+	redoCmd := &Redo{receiver: receiver}
+	caller.SetCommand(redoCmd)
+	assert.Equal(t, "'redo' run by receiver", caller.RunCommand())
 }
