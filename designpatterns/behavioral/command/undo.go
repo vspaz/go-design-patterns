7a6b73c4fdfc9c9ca947b5a4e9cb7fca451c3a14
vspaz <vladimir.edelman@gmail.com>
2021-12-03 21:00:54 +0100

    add missing methods

--- designpatterns/behavioral/command/undo.go
+++ designpatterns/behavioral/command/undo.go
@@ -3,3 +3,11 @@ package command
 type Undo struct {
 	Receiver Receiver
 }
+
+func (u *Undo) Undo(receiver Receiver) {
+	u.Receiver = receiver
+}
+
+func (u *Undo) RunCommand() string {
+	return u.Receiver.RunUndo()
+}
