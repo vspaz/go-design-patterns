b77fba4245fb874f323d5a3e03611f41ccfa59ac
vspaz <vladimir.edelman@gmail.com>
2021-12-16 17:18:53 +0100

    add missing methods

--- designpatterns/behavioral/template/hotdrink.go
+++ designpatterns/behavioral/template/hotdrink.go
@@ -6,3 +6,11 @@ type AbstractDrink interface {
 type HotDrink struct {
 	Drink AbstractDrink
 }
+
+func (h *HotDrink) BoilWater() string {
+	return "water boiled"
+}
+
+func (h *HotDrink) PourInCup() string {
+	return "poured in cup"
+}
