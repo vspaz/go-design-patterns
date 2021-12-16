de55ebde13a4446cb9245f715dc70e5cd7ecec7c
vspaz <vladimir.edelman@gmail.com>
2021-12-16 17:30:18 +0100

    add basic test case

--- designpatterns/behavioral/template/tea.go
+++ designpatterns/behavioral/template/tea.go
@@ -3,6 +3,10 @@ package template
 type Tea struct {
 }
 
+func (t *Tea) PrepareRecipe() string {
+	return t.SteepTeaBag() + t.AddLemon()
+}
+
 func (t *Tea) SteepTeaBag() string {
 	return "tea bag steeped"
 }
--- designpatterns/behavioral/template/template_test.go
+++ designpatterns/behavioral/template/template_test.go
@@ -1,7 +1,16 @@
 package template
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestTemplate(t *testing.T) {
-
+	tea := &Tea{}
+	hotTea := &HotDrink{
+		Drink: tea,
+	}
+	assert.Equal(t, "water boiled", hotTea.BoilWater())
+	assert.Equal(t, "tea bag steepedlemon added", hotTea.Drink.PrepareRecipe())
+	assert.Equal(t, "poured in cup", hotTea.PourInCup())
 }
