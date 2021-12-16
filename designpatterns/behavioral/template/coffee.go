cb676c6626f8d96f01c28918b6a8ef9bf36c66b9
vspaz <vladimir.edelman@gmail.com>
2021-12-16 17:13:50 +0100

    add 'coffee' methods

--- designpatterns/behavioral/template/coffee.go
+++ designpatterns/behavioral/template/coffee.go
@@ -1,1 +1,15 @@
 package template
+
+type Coffee struct{}
+
+func (c *Coffee) PrepareRecipe() string {
+	return c.BrewCoffee() + c.AddSugarAndMilk()
+}
+
+func (c *Coffee) BrewCoffee() string {
+	return "coffee brewed"
+}
+
+func (c *Coffee) AddSugarAndMilk() string {
+	return "sugar and milk added to coffee"
+}
