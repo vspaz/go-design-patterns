c318037434cdf72f409ee77e90da101a4e29c3d0
vspaz <vladimir.edelman@gmail.com>
2021-12-22 17:44:32 +0100

    add missing methods

--- designpatterns/behavioral/memento/originator.go
+++ designpatterns/behavioral/memento/originator.go
@@ -3,3 +3,27 @@ package memento
 type Originator struct {
 	state string
 }
+
+type Memento struct {
+	savedState string
+}
+
+func (o *Originator) GetState() string {
+	return o.state
+}
+
+func (o *Originator) SetState(state string) {
+	o.state = state
+}
+
+func (o *Originator) GetMemento() Memento {
+	return Memento{o.state}
+}
+
+func (m *Memento) GetState() string {
+	return m.savedState
+}
+
+func (o *Originator) Restore(memento Memento) {
+	o.state = memento.GetState()
+}
